bb10287618c8083a4b4a6b7a0ed333d945fc93cd
asyae <asya.elik5@etu.univ-lorraine.fr>
2025-11-22 19:32:23 +0100

    ajout du css films et film.php et suivi de son css

--- header.php
+++ header.php
@@ -16,9 +16,9 @@
         <img src="./image/logo.png" alt="logo">
 
 
-        <li><a href="index.html">Accueil</a></li>
+        <li><a href="index.php">Accueil</a></li>
         <li><a href="#">Votes</a></li>
-        <li><a href="films.html">Films</a></li>
+        <li><a href="films.php">Films</a></li>
         <li><a href="#">Résultats</a></li>
         <li><a href="#">Connexion</a></li>
     </ul>
