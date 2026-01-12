99abcb497d7c5bfb3e0f6e7d670d0be59cadb021
Frey101 <frey.alyssia67@gmail.com>
2026-01-12 18:42:49 +0100

    Création du CGU

--- views/cgu.php
+++ views/cgu.php
@@ -1,1 +1,44 @@
-dsfghjkiuytfdr
+<body>
+<h1> Modèle de Mentions Légales</h1>
+
+<div class="section">
+    <h2>1) Objet</h2>
+
+    <p>Les présentes CGU ont pour objet de définir les modalités de mise à disposition du site et les conditions d'utilisation par l'utilisateur. Toute consultation ou utilisation du site implique l'acceptation sans réserve des présentes conditions.</p>
+
+
+    <h2>2) Accès au site</h2>
+    <p>Le site est accessible gratuitement à tout utilisateur disposant d'un accès à internet. Les éditrices s'efforcent de maintenir le site accessible 24h/24 et 7j/7, mais ne peuvent être tenues responsables en cas d'interruption pour maintenance ou problème technique.</p>
+
+
+    <h2>3) Système de Vote</h2>
+
+    <p>Le site propose une fonctionnalité de vote. L'utilisateur s'engage à :</p>
+    <p>Ne voter qu'une seule fois par session (principe d'équité).</p>
+    <p>Ne pas tenter d'utiliser des scripts, des robots ou des méthodes de détournement technique pour fausser les résultats.</p>
+    <p>Respecter le caractère anonyme du vote. Les éditrices se réservent le droit d'annuler des votes ou de réinitialiser un scrutin en cas de détection de fraude.</p>
+
+    <h2>4) Formulaire de contact</h2>
+
+    <p>L'utilisateur s'engage à fournir des informations véridiques lors de l'utilisation du formulaire de contact. Il est interdit d'utiliser ce formulaire pour envoyer des messages injurieux, diffamatoires, spammeurs ou contraires aux lois en vigueur.</p>
+
+    <h2>5) Propriété Intellectuelle</h2>
+    <p>Le contenu du site (code source, design, textes) est protégé par le droit d'auteur. L'utilisateur n'est pas autorisé à copier, modifier ou distribuer les éléments du site sans l'accord préalable écrit d'Alyssia Frey et Asya Elik.</p>
+
+
+    <h2>6) Limitation de responsabilité</h2>
+
+    <p>Les éditrices ne sauraient être tenues responsables :</p>
+    <p>Des dommages directs ou indirects résultant de l'utilisation du site.</p>
+    <p>Des bugs informatiques ou de l'incompatibilité du site avec le matériel de l'utilisateur.</p>
+    <p>Des erreurs de saisie dans le formulaire de contact par l'utilisateur.</p>
+
+
+
+    <h2>Modification des CGU</h2>
+
+    <p>Alyssia Frey et Asya Elik se réservent le droit de modifier les présentes CGU à tout moment afin de les adapter aux évolutions du site ou de la législation.</p>
+
+</div>
+
+</body>
