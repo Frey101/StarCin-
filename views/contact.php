3c146fe3eba16673488f49ad39b03df6ad88e3fa
Frey101 <frey.alyssia67@gmail.com>
2026-01-18 11:47:44 +0100

    Modification css de contact

--- views/contact.php
+++ views/contact.php
@@ -3,27 +3,27 @@
     <div class ="form-container">
         <h2>Contactez-nous</h2>
         <form >
-            <div class="form-group">
+            <div class="form-group-contact">
                 <label for="nom">Nom</label>
                 <input type="text" id="nom" name="nom">
             </div>
-            <div class="form-group">
+            <div class="form-group-contact">
                 <label for="prenom">Prenom</label>
                 <input type="text" id="prenom" name="prenom">
             </div>
 
-            <div class="form-group">
+            <div class="form-group-contact">
                 <label for="email">Email</label>
                 <input type="email" id="email" name="email">
             </div>
 
-            <div class="form-group">
+            <div class="form-group-contact">
                 <label for="text">Téléphone</label>
                 <input type="number" id="telephone" name="telephone">
             </div>
 
 
-            <div class="form-group">
+            <div class="form-group-contact">
                 <label for="message">Message</label>
                 <textarea name="message" id="message" cols="30" rows="10"></textarea>
             </div>
